940d1a6dc97c5ec318036093f29f54a61d86aed9
guilhermescr <guilhermechaves2005@hotmail.com>
2022-05-15 13:18:13 -0300

    to-do-list concluída

--- exercises/js/assets/js/scripts.js
+++ exercises/js/assets/js/scripts.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+var resetBtn = document.getElementById('reset');
+
+function increment() {
+  if (currentNumber < 10) {
+    currentNumber ++;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
+}
+
+function decrement() {
+  if (currentNumber > 0) {
+    currentNumber --;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
+}
+
+function resetCounter() {
+  currentNumber = 0;
+  currentNumberWrapper.innerHTML = currentNumber;
+}

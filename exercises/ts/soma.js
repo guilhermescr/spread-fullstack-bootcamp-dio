145a61b372403fb7e62033579f346a415906a036
guilhermescr <guilhermechaves2005@hotmail.com>
2022-06-14 14:59:16 -0300

    meus primeiros códigos em typescript

---
+++ exercises/ts/soma.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 3;
+let b = 78;
+function somaNums(a, b) {
+    return a + b;
+}
+console.log(`${a} + ${b} = ${somaNums(a, b)}`);
---
+++ exercises/ts/soma.ts
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 78;
+
+function somaNums(a: number, b: number) {
+  return a + b;
+}
+
+console.log(`${a} + ${b} = ${somaNums(a, b)}`);
